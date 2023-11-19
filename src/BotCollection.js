ca36521fcbabce406d8f23242e72026631821c2d
MainaZaquir <mzaquir58@gmail.com>
2023-11-19 13:10:04 +0300

    Updated BotCollection.js

---
+++ src/BotCollection.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BotCard from './BotCard';
+
+const BotCollection = ({ bots, selectBot }) => (
+    <div>
+        {bots.map(bot => <BotCard key={bot.id} bot{bot} selectBot{selectBot} />)}
+    </div>
+);
+
+
+export default BotCollection
