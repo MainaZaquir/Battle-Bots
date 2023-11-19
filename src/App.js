0b62c2ff1f8b013b8b9fc2aa9401d7fed13a39cd
MainaZaquir <mzaquir58@gmail.com>
2023-11-19 13:03:31 +0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import BotCollection from './BotCollection';
+import BotArmy from './BotArmy';
+import BotSpecs from './BotSpecs';
+import SortBot from './SortBot';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    bots: [],
+    army: [],
+    selectedBot: null
+  };
+
+  componentDidMount() {
+    // Fetching the bots data from teh JSON server and setting it to state
+    fetch('https://localhost:3000/bots')
+      .then(Response => Response.json())
+      .then(bots => this.setState({ bots }));
+  }
+
+  addBotToArmy = (bot) => {
+    // Checking if teh army already includes a bot from the same class
+    if (!this.state.army.some(b => b.bot_class === bot.bot_class)) {
+      this.setState(prevState => ({
+        army: [...prevState.army, bot],
+        //Removing the bot from the BotCollection
+        bots: prevState.bots.filter(b => b.id !== bot.id)
+      }));
+    }
+  };
+
+  releaseBot = (bot) => {
+    this.setState(prevState => ({
+      army: prevState.army.filter(b => b.id !== bot.id),
+      // Adding the bot back to teh BotCollection
+      bots: [...prevState.bots, bot]
+    }));
+  };
+
+  dischargeBot = (bot) => {
+    //Discharging the bot
+    fetch('https://localhost:3000/bots/${bot.id}', { method: 'DELETE' })
+      .then(() => {
+        this.setState(prevState => ({
+          bots: prevState.bots.filter(b => b.id !== bot.id),
+          army: prevState.army.filter(b => b.id !== bot.id)
+        }));
+      });
+  };
+
+  selectBot = (bot) => {
+    //Setting the  selcted bot
+    this.setState({ selectedBot: bot });
+  };
+
+  sortBots = (criteria) => {
+    //Sorting the bots
+    this.setState(prevState => ({
+      bots: prevState.bots.sort((a, b) => b[criteria] - a[criteria])
+    }));
+  };
+
+  filterBots = (botClass) => {
+    //Filtering the bots
+    this.setState(prevState => ({
+      bots: prevState.bots.filter(bot => bot.bot_class === botClass)
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <SortBar sortBots={this.sortBots} filterBots={this.filterBots} />
+        {this.state.selectedBot ? (
+          <BotSpecs bot={this.state.selectedBot} addBotToArmy={this.addBotToArmy} />
+        ) : (
+          <BotCollection bots={this.state.bots} selectBot={this.selectBot} />
+        )}
+        <BotArmy army={this.state.army} releaseBot={this.releaseBot} />
+      </div>
+    );
+  }
 }
 
 export default App;
