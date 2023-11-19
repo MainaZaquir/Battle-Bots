0d7325efb3b0a67b4eca34f0e33b0000ac616604
MainaZaquir <mzaquir58@gmail.com>
2023-11-19 13:31:11 +0300

    Updated components

---
+++ src/components/SortBot.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SortBots = ({ sortBots, filterBots }) => (
+    <div>
+        <button onClick={() => sortBots('health')}>Sort by Health</button>
+        <button onClick={() => sortBots('damage')}>Sort by Damage</button>
+        <button onClick={() => sortBots('armor')}>Sort by Armor</button>
+        <button onClick={() => sortBots('name')}>Sort by Name</button>
+        <button onClick={() => filterBots('Support')}>Filter by Support</button>
+    </div>
+);
+
+export default SortBots;
